33bbedaebf45a6955ba22204e073ca5e6109942c
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2022-11-11 15:10:57 +0530

    [Added: Ability to print files from file]

--- src/com/readWrite/EmployeePayrollService.java
+++ src/com/readWrite/EmployeePayrollService.java
@@ -1,25 +1,22 @@
 package com.readWrite;
 
 import java.io.IOException;
+
 import java.nio.file.Files;
+
 import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
-import java.util.ArrayList;
+
+import java.util.List;
 
 public class EmployeePayrollService {
 
 	public void listFilesDirectory() throws IOException, InterruptedException {
-		ArrayList<String> writing = new ArrayList<String>();
 
-		writing.add("Id : 1001");
-		writing.add("Name : Nadeem Akhtar");
-		writing.add("Salary : 84761");
-		// File Exist => Override
-		// Will create new file if not exist
-		Files.write(Paths.get(
-				"C:\\Users\\menad\\OneDrive\\Desktop\\Blabz practice problem\\Day2Problems\\Day-27-IO-ReadWrite\\src\\testdemo.txt"),
-				writing, StandardOpenOption.CREATE);
-		System.out.println("Exporting Into File");
+		List<String> line = Files.readAllLines(Paths.get(
+				"C:\\Users\\menad\\OneDrive\\Desktop\\Blabz practice problem\\Day2Problems\\Day-27-IO-ReadWrite\\src\\testdemo.txt"));
+		for (String lines : line) {
+			System.out.println(lines);
+		}
 	}
 
 	public static void main(String[] args) throws IOException, InterruptedException {
