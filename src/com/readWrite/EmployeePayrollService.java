e120e69f9bf33aff07b2d07448b49673523846ee
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2022-11-11 15:49:59 +0530

    [Added: Ability to print the Employee Payroll from file]

--- src/com/readWrite/EmployeePayrollService.java
+++ src/com/readWrite/EmployeePayrollService.java
@@ -1,5 +1,6 @@
 package com.readWrite;
 
+import java.io.File;
 import java.io.IOException;
 
 import java.nio.file.Files;
@@ -9,18 +10,19 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class EmployeePayrollService {
+	public long listFilesDirectory() throws IOException, InterruptedException {
 
-	public void listFilesDirectory() throws IOException, InterruptedException {
-
-		List<String> line = Files.readAllLines(Paths.get(
-				"C:\\Users\\menad\\OneDrive\\Desktop\\Blabz practice problem\\Day2Problems\\Day-27-IO-ReadWrite\\src\\testdemo.txt"));
-		for (String lines : line) {
-			System.out.println(lines);
-		}
+		long enteries = 0;
+		enteries = Files.lines(new File(
+				"C:\\Users\\menad\\OneDrive\\Desktop\\Blabz practice problem\\Day2Problems\\Day-27-IO-ReadWrite\\src\\testdemo.txt")
+				.toPath()).count();
+		System.out.println(enteries);
+		return enteries;
 	}
 
 	public static void main(String[] args) throws IOException, InterruptedException {
-		EmployeePayrollService obj = new EmployeePayrollService();
-		obj.listFilesDirectory();
+		EmployeePayrollService emp = new EmployeePayrollService();
+		emp.listFilesDirectory();
+
 	}
 }
