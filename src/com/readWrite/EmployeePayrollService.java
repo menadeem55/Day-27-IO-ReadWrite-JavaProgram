8373476cbf224743ad10042aef9da47f8701d699
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2022-11-11 14:52:14 +0530

    [Added: Creating Watch Service program]

--- src/com/readWrite/EmployeePayrollService.java
+++ src/com/readWrite/EmployeePayrollService.java
@@ -1,30 +1,54 @@
 package com.readWrite;
 
 import java.io.IOException;
+import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.nio.file.WatchService;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchKey;
+
+import static java.nio.file.StandardWatchEventKinds.*;
 
 public class EmployeePayrollService {
 
-	public void empPayrollRead() throws IOException {
-		Path playPath = Paths.get(
-				"C:\\Users\\menad\\OneDrive\\Desktop\\Blabz practice problem\\Day2Problems\\Day-27-IO-ReadWrite\\src\\com");
+	public void listFilesDirectory() throws IOException, InterruptedException {
 
-		Files.list(playPath).filter(Files::isRegularFile).forEach(System.out::print);
-		Files.newDirectoryStream(playPath).forEach(System.out::println);
-		Files.newDirectoryStream(playPath, path -> path.toFile().isFile() && path.toString().startsWith("temp"));
+		WatchService watchService = FileSystems.getDefault().newWatchService(); // Activate watch Service
 
+		Path path = Paths.get("C:\\Users\\menad\\OneDrive\\Desktop\\WatchService\\"); // Convert location to file path
+
+		// Register watch service on above path ==> Pass events to be watched
+		// (Create/Delete/Modify)
+		path.register(watchService, ENTRY_CREATE, ENTRY_DELETE, ENTRY_MODIFY);
+
+		List<String> logs = new ArrayList<String>();
+
+		boolean poll = true;
+		while (poll) {
+
+			WatchKey key = watchService.take(); // Reference / Pointer current file or folder watching
+
+			for (WatchEvent<?> event : key.pollEvents()) { // Printing the events (Create / Modify / Delete)
+				System.out.println("Event kind : " + event.kind() + " - File : " + event.context());
+
+				logs.add("Event kind : " + event.kind() + " - File : " + event.context());
+			}
+
+			Files.write(Paths.get("C:\\Users\\menad\\OneDrive\\Desktop\\WatchService\\"), logs,
+					StandardOpenOption.CREATE);
+
+			poll = key.reset(); // Reseting the reference so that it can be used later
+		}
 	}
 
-	public static void main(String[] args) throws IOException {
-		EmployeePayrollService emp = new EmployeePayrollService();
-
-		emp.empPayrollRead();
+	public static void main(String[] args) throws IOException, InterruptedException {
+		EmployeePayrollService obj = new EmployeePayrollService();
+		obj.listFilesDirectory();
 	}
 
 }
